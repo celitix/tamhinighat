9da90fce2d829ba745c492116b2f61446f201876
celitix <developer@proactivedigital.in>
2026-02-25 14:35:34 +0530

    Add canonical URL to layout metadata
    Added canonical URL to metadata for SEO.

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,6 +1,9 @@
 export const metadata = {
   title: "Western Ghats | Gold Valley Tamhini Ghat | Tamhini Ghat Information",
   description: "The gold valley Tamhini Ghat is a pride of the Western Ghats.All tamhini ghat information and know about refer www.tamhinighat.com",
+  alternates: {
+    canonical: "https://www.tamhinighat.com/",
+  },
 };
 
 export default function Layout({ children }) {
